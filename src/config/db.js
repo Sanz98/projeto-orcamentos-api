4e1d2d0bd1ef8fb926b63c60dbe028d76049d361
pmartins-dev <patrick.s.martins02@gmail.com>
2025-11-22 16:44:15 -0300

    Atualizando banco de dados

---
+++ src/config/db.js
@@ -0,0 +1,29 @@
+// Importa a biblioteca para conectar ao SQL Server.
+const sql = require('mssql');
+require('dotenv').config();
+
+// Objeto com as configurações de acesso ao banco de dados.
+const CONFIG ={
+    user: process.env.DB_USER,
+    password:process.env.DB_PASS,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    options:{
+        encrypt: true,
+        trustServerCertificate: true //Ignora o erro de certificação autoassinado
+    }
+}
+
+// Função assíncrona que estabelece e retorna um pool de conexões.
+async function getConnection(){
+    try {
+        // Tenta conectar ao banco usando as configurações definidas.
+        const pool = await sql.connect(CONFIG);
+        return pool;
+    } catch (error) {
+        console.error('Erro na conexão SQL Serve:', error);
+    }
+}
+
+// Exporta a função e a biblioteca para serem usadas em outras partes do projeto.
+module.exports ={sql, getConnection};
