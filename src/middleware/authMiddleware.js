fab883ca6572b24fab851d614160d04e9dfdc785
unknown <patrick.s.martins02@gmail.com>
2025-12-06 13:43:52 -0300

    atualizando

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,40 @@
 const jwt = require('jsonwebtoken');
 
 const verify = {
+        // Middleware para verificar se o usuário está logado (independente do perfil)
+    autenticado: async (req, res, next) => {
+        try {
+            // 1. Tenta pegar SOMENTE do Cookie
+            const token = req.cookies.token;
+
+            // Se não tiver cookie, lança erro
+            if (!token) {
+                throw new Error('Não autorizado. Cookie de sessão não encontrado.');
+            }
+            
+            // 2. Verifica o Token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+
+            // 3. Anexa os dados do usuário para o próximo Controller
+            // IMPORTANTE: Aqui não fazemos if/else de perfil. Apenas passamos os dados.
+            req.usuario = { 
+                idUsuario: decoded.idUsuario, 
+                perfil: decoded.perfil 
+            };
+
+            next(); // Permissão concedida: O usuário existe e o token é válido.
+
+        } catch (error) {
+            console.error('Erro na Autenticação:', error.message);
+            
+            if (error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
+                return res.status(401).json({ erro: 'Sessão expirada ou inválida. Refaça o login.' });
+            }
+            
+            return res.status(401).json({ erro: 'Você precisa estar logado para acessar este recurso.' });
+        }
+    },
+
     gerente: async (req, res, next) => {
         try {
             // 1. Tenta pegar SOMENTE do Cookie
@@ -8,7 +42,7 @@ const verify = {
 
             // Se não tiver cookie, já lança erro
             if (!token) {
-                throw new Error('Não autorizado. Cookie de sessão não encontrado.');
+                return res.status(401).json({erro: "Acesso não autorizado - Realize login!"});
             }
             
             const decoded = jwt.verify(token, process.env.JWT_SECRET); 
